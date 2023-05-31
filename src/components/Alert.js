227f7b1356dfb745a377554fc72edeb2b3522a54
ayushsachan123 <ayushsachan.in@gmail.com>
2023-06-01 02:31:49 +0530

    add Alert.js

---
+++ src/components/Alert.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function Alert(props) {
+
+  const capitalize = (word) => {
+    const lower = word.toLowerCase();
+    return lower.charAt(0).toUpperCase() + lower.slice(1);
+  };
+
+  return (
+    <div style={{ marginTop : "56px",height: "50px" }}>
+      {props.alert && (
+        <div
+          className={`alert alert-${props.alert.type} alert-dismissible fade show`}
+          role="alert"
+        >
+          <strong> {capitalize(props.alert.type)}</strong>: {props.alert.msg}
+        </div>
+      )}
+    </div>
+  );
+}
+
